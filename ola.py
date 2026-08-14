519617e3ac7d6a23776f51f7fa1fac225f8a7cf5
pedrosalviano22-crypto <pedrosalviano22@gmail.com>
2026-08-14 15:42:04 -0300

    Update ola.py

--- ola.py
+++ ola.py
@@ -1,1 +1,19 @@
-print("ola, mundo")
+print("seja bem vindo!")
+conta=input("gostaria de criar um cadastro? (sim/não)")
+if =="sim":
+email=input("digite seu email:")
+print("casdastro com sucesso")
+nome=input("qual seu primeiro e segundo nome:")
+print("cadastrado com sucesso")
+idade=input("qual sua idade?")
+inf>=18:
+print("cadastrado com sucesso")
+else:
+prin("você precisa ter 18 ou mais pra cadastrar")
+senha=input("digite seua senha:")
+print("cadastrado com sucesso")
+
+
+
+
+
